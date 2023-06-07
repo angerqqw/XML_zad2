515f19967a789e75e9f73fb7e6d2aba4ea389708
angerqqw <135815376+angerqqw@users.noreply.github.com>
2023-06-07 11:33:22 +0300

    Add files via upload

---
+++ WinFormsApp31/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WinFormsApp31
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            dataGridView1 = new DataGridView();
+            name = new DataGridViewTextBoxColumn();
+            Age = new DataGridViewTextBoxColumn();
+            Programmer = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(258, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Age";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(526, 49);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Programmer";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(67, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(146, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(311, 42);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "True", "False" });
+            comboBox1.Location = new Point(624, 41);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(128, 28);
+            comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(67, 111);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 33);
+            button1.TabIndex = 6;
+            button1.Text = "Добавить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(298, 111);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 33);
+            button2.TabIndex = 7;
+            button2.Text = "Редактировать";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(562, 111);
+            button3.Name = "button3";
+            button3.Size = new Size(159, 33);
+            button3.TabIndex = 8;
+            button3.Text = "Удалить";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(48, 409);
+            button4.Name = "button4";
+            button4.Size = new Size(187, 29);
+            button4.TabIndex = 9;
+            button4.Text = "Сохранить как XML";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(298, 409);
+            button5.Name = "button5";
+            button5.Size = new Size(156, 29);
+            button5.TabIndex = 10;
+            button5.Text = "Очистить таблицу";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(544, 409);
+            button6.Name = "button6";
+            button6.Size = new Size(177, 29);
+            button6.TabIndex = 11;
+            button6.Text = "Загрузить XML";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { name, Age, Programmer });
+            dataGridView1.Location = new Point(33, 168);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(708, 213);
+            dataGridView1.TabIndex = 12;
+            // 
+            // name
+            // 
+            name.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            name.HeaderText = "Name";
+            name.MinimumWidth = 6;
+            name.Name = "name";
+            // 
+            // Age
+            // 
+            Age.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Age.HeaderText = "Age";
+            Age.MinimumWidth = 6;
+            Age.Name = "Age";
+            // 
+            // Programmer
+            // 
+            Programmer.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Programmer.HeaderText = "Programmer";
+            Programmer.MinimumWidth = 6;
+            Programmer.Name = "Programmer";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn Age;
+        private DataGridViewTextBoxColumn Programmer;
+    }
+}
